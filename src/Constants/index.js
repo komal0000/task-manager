61168349db4befcfa3f98491bd5897959bb84bbf
chhatraman shrestha <cms111000111@gmail.com>
2024-08-16 09:42:20 +0545

    index managed

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,5 +1,4 @@
 export const statuses = ["Pending", "Seen", "Working","Ontest", "Tested", "Finished", "Updated", "Backlog"];
 export const getCollectionName=()=>{
-    // return process.env.NODE_ENV === 'development' ? 'task_test' : 'tasks';
-    return 'tasks';
+    return process.env.NODE_ENV === 'development' ? 'task_test' : 'tasks';
 };
