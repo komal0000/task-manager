52c879314197d7be365cb5216e5375fac84ff180
chhatraman shrestha <cms111000111@gmail.com>
2024-08-25 17:54:49 +0545

    cloudnary fixed

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -4,14 +4,16 @@ export const statuses = ["Pending", "Seen", "Working","Ontest", "Tested", "Finis
 export const getCollectionName=()=>{
     return process.env.NODE_ENV === 'development' ? 'task_test' : 'tasks';
 };
-
+export const getCloudnarykeyName=()=>{
+    return process.env.NODE_ENV === 'development' ? 'task_test' : 'tasks';
+};
 export const uploadImageToCloudinary = async (images) => {
     const uploads = images.map((image) => {
       const formData = new FormData();
       formData.append("file", image);
-      formData.append("upload_preset", "fp5cicub"); // Your Cloudinary upload preset
+      formData.append("upload_preset", "hg8nljf5"); // Your Cloudinary upload preset
       return axios.post(
-        `https://api.cloudinary.com/v1_1/dwclw3s0d/image/upload`, // Corrected endpoint
+        `https://api.cloudinary.com/v1_1/drpcwhlnp/image/upload`, // Corrected endpoint
         formData
       ).then(res => res.data.url);
     });
