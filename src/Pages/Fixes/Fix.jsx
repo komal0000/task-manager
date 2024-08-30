5774599ec609a2dbcb4c5c2a609f0fc245c50337
roshanlimbu <birenlimbu415@gmail.com>
2024-08-30 13:11:12 +0545

    fix: updated ui

--- src/Pages/Fixes/Fix.jsx
+++ src/Pages/Fixes/Fix.jsx
@@ -1,29 +1,35 @@
-import { collection, doc, getDocs, onSnapshot, serverTimestamp, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect } from "react";
 import db from "../../firebase";
 import { getCollectionName } from "../../Constants";
 
-const Fix=()=>{
-    useEffect(() => {
-       getDocs(collection(db, getCollectionName()))
-       .then((snapshot) => {
+const Fix = () => {
+  useEffect(() => {
+    getDocs(collection(db, getCollectionName()))
+      .then((snapshot) => {
         snapshot.forEach(async (documentSnapshot) => {
-            const data = documentSnapshot.data();
-            if (!data.created_at || !data.updated_at) {
-              const docRef = doc(db, getCollectionName(), documentSnapshot.id);
-              await updateDoc(docRef, {
-                created_at: data.created_at || serverTimestamp(),
-                updated_at: data.updated_at || serverTimestamp(),
-              });
-  
-              console.log(`Updated document with ID: ${documentSnapshot.id}`);
-            }
-          });
-       }).catch((err) => {
-        
-       });
-    
-      }, []);
-}
+          const data = documentSnapshot.data();
+          if (!data.created_at || !data.updated_at) {
+            const docRef = doc(db, getCollectionName(), documentSnapshot.id);
+            await updateDoc(docRef, {
+              created_at: data.created_at || serverTimestamp(),
+              updated_at: data.updated_at || serverTimestamp(),
+            });
+
+            console.log(`Updated document with ID: ${documentSnapshot.id}`);
+          }
+        });
+      })
+      .catch((err) => {});
+  }, []);
+};
 
 export default Fix;
+
